f1496dba5736e33b99f49c5e88db7c846bff0b39
1dominik7 <dominik-wicek@o2.pl>
2023-10-27 16:40:11 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .env
    	new file:   prisma/migrations/20231020080822_first/migration.sql
    	new file:   prisma/migrations/20231020094529_add_user/migration.sql
    	new file:   prisma/migrations/20231020122502_restaurant/migration.sql
    	new file:   prisma/migrations/migration_lock.toml
    	new file:   prisma/schema.prisma
    	new file:   public/1.png
    	new file:   public/2.png
    	new file:   public/3.png
    	new file:   public/4.png
    	new file:   public/apps.jpg
    	new file:   public/cart.png
    	new file:   public/close.png
    	new file:   public/contact.png
    	new file:   public/delete.png
    	new file:   public/edit.png
    	new file:   public/facebook.png
    	new file:   public/google.png
    	new file:   public/hero.png
    	new file:   public/illustration.png
    	new file:   public/loginBg.png
    	new file:   public/offerBg.png
    	new file:   public/offerProduct.png
    	new file:   public/open.png
    	new file:   public/phone.png
    	new file:   public/slide1.png
    	new file:   public/slide2.png
    	new file:   public/slide3.jpg
    	new file:   public/temporary/m1.png
    	new file:   public/temporary/m2.png
    	new file:   public/temporary/m3.png
    	new file:   public/temporary/p1.png
    	new file:   public/temporary/p10.png
    	new file:   public/temporary/p11.png
    	new file:   public/temporary/p12.png
    	new file:   public/temporary/p13.png
    	new file:   public/temporary/p14.png
    	new file:   public/temporary/p15.png
    	new file:   public/temporary/p16.png
    	new file:   public/temporary/p17.png
    	new file:   public/temporary/p18.png
    	new file:   public/temporary/p19.png
    	new file:   public/temporary/p2.png
    	new file:   public/temporary/p3.png
    	new file:   public/temporary/p4.png
    	new file:   public/temporary/p5.png
    	new file:   public/temporary/p6.png
    	new file:   public/temporary/p7.png
    	new file:   public/temporary/p8.png
    	new file:   public/temporary/p9.png
    	new file:   public/upload.png
    	new file:   public/websites.jpg
    	new file:   src/app/add/page.tsx
    	new file:   src/app/api/(checkout)/confirm/[intentId]/route.ts
    	new file:   src/app/api/(checkout)/create-intent/[orderId]/route.ts
    	new file:   src/app/api/auth/[...nextauth]/route.ts
    	new file:   src/app/api/categories/route.ts
    	new file:   src/app/api/orders/[id]/route.ts
    	new file:   src/app/api/orders/route.ts
    	new file:   src/app/api/products/[id]/route.ts
    	new file:   src/app/api/products/route.ts
    	new file:   src/app/cart/page.tsx
    	new file:   src/app/components/AddressForm.tsx
    	new file:   src/app/components/AuthProvider.tsx
    	new file:   src/app/components/CartIcon.tsx
    	new file:   src/app/components/CheckoutForm.tsx
    	new file:   src/app/components/CountDown.tsx
    	new file:   src/app/components/DeleteButton.tsx
    	new file:   src/app/components/Featured.tsx
    	new file:   src/app/components/Footer.tsx
    	new file:   src/app/components/Menu.tsx
    	new file:   src/app/components/Navbar.tsx
    	new file:   src/app/components/Notification.tsx
    	new file:   src/app/components/Offer.tsx
    	new file:   src/app/components/Price.tsx
    	new file:   src/app/components/QueryProvider.tsx
    	new file:   src/app/components/Slider.tsx
    	new file:   src/app/components/UserLinks.tsx
    	new file:   src/app/data.ts
    	new file:   src/app/login/page.tsx
    	new file:   src/app/menu/[category]/page.tsx
    	new file:   src/app/menu/page.tsx
    	new file:   src/app/orders/page.tsx
    	new file:   src/app/pay/[id]/page.tsx
    	new file:   src/app/product/[id]/page.tsx
    	new file:   src/app/success/page.tsx
    	new file:   src/docker/docker-compose.yml
    	new file:   src/types/types.ts
    	new file:   src/utils/auth.ts
    	new file:   src/utils/connect.ts
    	new file:   src/utils/store.ts

---
+++ src/app/components/Featured.tsx
@@ -0,0 +1,43 @@
+import {ProductType} from '@/types/types'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const getData = async () =>{
+  const res = await fetch("http://localhost:3000/api/products",{
+    cache: "no-store"
+  })
+
+  if(!res.ok){
+    throw new Error("Failed!")
+  }
+
+  return res.json()
+}
+
+const Featured = async () => {
+
+const featuredProducts: ProductType[] = await getData()
+
+  return (
+    <div className='w-screen overflow-x-scroll text-red-500'>
+        <div className='w-max flex'>
+            {featuredProducts.map(item => (
+            <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]' key={item.id}>
+               {item.img && 
+               <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration 500' key={item.id}>
+                    <Image src={item.img} alt="" fill className='object-contain'/>
+                </div>}
+                <div className='flex-1 flex flex-col items-center justify-center text-center gap-4 z-10'>
+                    <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl'>{item.title}</h1>
+                    <p className='p-4 2xl:p-8'>{item.desc}</p>
+                    <span className='text-xl font-bold'>$ {item.price}</span>
+                    <Link href={`/product/${item.id}`} className='bg-red-500 text-white p-2 rouded-md'>Add to Cart</Link>
+                </div>
+            </div>))}
+        </div>
+    </div>
+  )
+}
+
+export default Featured
